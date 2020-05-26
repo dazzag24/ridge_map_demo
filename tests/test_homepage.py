58e617cff750f2957f0364e48c8ee6f9cfee565c
darreng <dazzag@gmail.com>
2020-05-26 14:32:28 +0100

    Fixing some flake8 warnings in test

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,5 +1,4 @@
-import pytest 
-
+#import pytest 
 import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from main import app
@@ -15,7 +14,8 @@ def test_home_page():
     assert response.status_code == 200
     assert b"Hello Ridgemap World!!!" in response.data
 
-def test_ridge_map():
+
+def test_ridge_map(): 
     response = app.test_client().get('/ridge_map?inputcoords=-3.529,54.1743,-2.5128,54.6998&label=Lakes')
     assert response.status_code == 200
     assert response.mimetype == 'image/png'
