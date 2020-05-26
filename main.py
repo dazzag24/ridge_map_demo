0c2fd4d8e37c819149cb4c754b01e722070977a2
darreng <dazzag@gmail.com>
2020-05-26 14:28:03 +0100

    Fixing some flake8 warnings

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ def ridge_map():
     print("Label: {}".format(label))
 
     coords = []
-    for c in list(map(lambda s:s.strip(), inputcoords.split(','))): 
-        coords.append(float(c)) 
+    for c in list(map(lambda s:s.strip(), inputcoords.split(','))):
+        coords.append(float(c))
 
-    rm = RidgeMap((coords[0],coords[1],coords[2],coords[3]))
+    rm = RidgeMap((coords[0], coords[1], coords[2], coords[3]))
     values = rm.get_elevation_data(num_lines=80)
     rm.plot_map(values=rm.preprocess(values=values, water_ntile=12, vertical_ratio=60),
-            label=label, 
-            label_x=0.75,
-            label_size=80,
-            linewidth=2,
-            line_color='orange')
+                label=label, 
+                label_x=0.75,
+                label_size=80,
+                linewidth=2,
+                line_color='orange')
 
     png_output = BytesIO()
     plt.savefig(png_output)
