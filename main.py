0ed43811be34f6d8d46f17e7f16b8264107dbb28
darreng <dazzag@gmail.com>
2020-05-26 14:24:52 +0100

    Fixing some linting warnings

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-  return "Hello Ridgemap World!!!"
+    return "Hello Ridgemap World!!!"
+
 
 @app.route("/ridge_map", methods=['get'])
-def ridge_map():
+def ridge_map(): 
     from io import BytesIO
     import matplotlib.pyplot as plt
     from ridge_map import RidgeMap
@@ -18,13 +20,13 @@ def ridge_map():
     print("Label: {}".format(label))
 
     coords = []
-    for c in list(map(lambda s:s.strip(), inputcoords.split(','))):
-      coords.append(float(c)) 
+    for c in list(map(lambda s:s.strip(), inputcoords.split(','))): 
+        coords.append(float(c)) 
 
     rm = RidgeMap((coords[0],coords[1],coords[2],coords[3]))
     values = rm.get_elevation_data(num_lines=80)
     rm.plot_map(values=rm.preprocess(values=values, water_ntile=12, vertical_ratio=60),
-            label=label,  
+            label=label, 
             label_x=0.75,
             label_size=80,
             linewidth=2,
@@ -37,6 +39,6 @@ def ridge_map():
   
     return response
 
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
-
